4a127f9c20731ed97cdefff0aeefe79f6c76ea42
Sandor <sanchez.hernandez.christian@gmail.com>
2024-10-09 09:58:00 -0600

    App Maui Added

---
+++ BlazorMAUIIdentity.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorMAUIIdentity.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ BlazorMAUIIdentity.Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorMAUIIdentity.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
